4a33ab81342abe2b2f4e6502403accca091e1000
charlie foulk <charlesfoulk@csus.edu>
2023-09-21 12:53:19 -0700

    temporary patch route for submissions

--- src/app/api/submissions/route.ts
+++ src/app/api/submissions/route.ts
@@ -4,7 +4,7 @@ import UserData from "@/utils/userData";
 import { dbConnect } from "@/utils/db";
 import { ZodError } from "zod";
 import { getErrorRes } from "@/lib/helpers";
-import { LEETCODE_GRAPHQL_URL } from "@/utils/consts";
+import { LEETCODE_GRAPHQL_URL, API_ROUTES } from "@/utils/consts";
 
 export async function POST(req: NextRequest, res: NextResponse) {
     console.log("POST submissions");
@@ -13,32 +13,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
         let username: string = body.username;
         console.log(body);
 
-        const opts = {
-            method: "POST",
-            headers: { "content-type": "application/json" },
-            body: JSON.stringify({
-                query: `
-              { matchedUser(username: "${username}") {
-                username
-                submitStats: submitStatsGlobal {
-                  acSubmissionNum {
-                    difficulty
-                    count
-                    submissions
-                  }
-                  totalSubmissionNum {
-                    difficulty
-                    count
-                    submissions
-                  }
-                }
-              }
-            }`,
-            }),
-        };
-        
-        const response = await fetch(`${LEETCODE_GRAPHQL_URL}/${username}`, opts);
-        const newData = await response.json();
+        const newData = await getLeetcode(username);
 
         // could be my problem here, the newData variablle isn't finished yet
         if (!newData) {
@@ -59,7 +34,7 @@ export async function POST(req: NextRequest, res: NextResponse) {
         // console.log(submission);
 
         return NextResponse.json(
-            { msg: "success", username: username, submission_id: submission._id }, 
+            { msg: "success", username: username, submission_id: submission._id },
             { status: 201 }
         );
 
@@ -73,3 +48,92 @@ export async function POST(req: NextRequest, res: NextResponse) {
         return getErrorRes(500, err.message);
     }
 }
+
+export async function PATCH(req: NextRequest, res: NextResponse) {
+    console.log("PATCH submissions");
+    try {
+        const body = await req.json();
+        let username: string = body.username;
+        console.log(body);
+
+        const newData = await getLeetcode(username);
+
+        // could be my problem here, the newData variablle isn't finished yet
+        if (!newData) {
+            return getErrorRes(500, "Failed to fetch Leetcode data");
+        }
+
+        const { conn, Submission } = await dbConnect();
+        const newSubmission = new Submission({
+            username: newData.data.matchedUser.username,
+            easy_solved: newData.data.matchedUser.submitStats.acSubmissionNum[1].count,
+            medium_solved: newData.data.matchedUser.submitStats.acSubmissionNum[2].count,
+            hard_solved: newData.data.matchedUser.submitStats.acSubmissionNum[3].count,
+        });
+
+        const filter = { username: username }
+        const update = {
+            easy_solved: 1000,
+            medium_solved: 2000,
+            hard_solved: 500,
+        }
+
+        await conn;
+        const submission = await newSubmission.updateOne(filter, update);
+        console.log(submission);
+
+        return NextResponse.json(
+            { msg: "success", username: username, modifiedCount: submission.modifiedCount },
+            { status: 201 }
+        );
+
+    } catch (err: any) {
+        if (err instanceof ZodError) {
+            return getErrorRes(400, "Validations failed", err);
+        }
+        if (err.code === "P2002") { // figure this out
+            return getErrorRes(409, "lasifasfhdsafadsf?");
+        }
+        return getErrorRes(500, err.message);
+    }
+}
+
+export async function getLeetcode(username: string) {
+
+    const opts = {
+        method: "POST",
+        headers: { "content-type": "application/json" },
+        body: JSON.stringify({
+            query: `
+              { matchedUser(username: "${username}") {
+                username
+                submitStats: submitStatsGlobal {
+                  acSubmissionNum {
+                    difficulty
+                    count
+                    submissions
+                  }
+                  totalSubmissionNum {
+                    difficulty
+                    count
+                    submissions
+                  }
+                }
+              }
+            }`,
+        }),
+    };
+
+    const response = await fetch(`${LEETCODE_GRAPHQL_URL}/${username}`, opts);
+
+    // const response = await fetch(`${API_ROUTES.leetcode}/${username}`);
+    
+    // const userData = new UserData;
+    // const Data = await userData.fetchLeetcodeData(username);
+    
+    const newData = await response.json();
+
+    return newData;
+
+}
+
